eaf9b01a305a2bfdf61c28d7b6bb00e828727f9f
AMANDA <amandasavluchinske@gmail.com>
2018-07-06 11:03:58 -0300

    Created the store

--- src/index.js
+++ src/index.js
@@ -6,3 +6,19 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+function createStore () {
+    //The store should have 4 parts
+    // 1. The state
+    // 2. Get the state
+    // 3. Listen to changes
+    // 4. Update state
+
+    let state
+
+    const getState = () => state
+
+    return {
+        getState
+    }
+}
